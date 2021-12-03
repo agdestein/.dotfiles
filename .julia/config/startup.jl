5efbe6a9d08469cfc6210bc1316fa0591faf11db
Syver Døving Agdestein <syverda@icloud.com>
2021-12-03 12:35:29 +0100

    Asynchronous revision

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,2 +1,9 @@
-using Revise
-
+atreplinit() do repl
+    @async try
+        sleep(0.1)
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+    catch
+        @warn("Could not load Revise.")
+    end
+end
